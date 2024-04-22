3b447644bd3b32d11a1a77c0c43ddb2e07fa585b
jakubbonczyk <jakubbonczyk@icloud.com>
2024-04-22 14:04:47 +0200

    First project changes

--- src/main/java/github/com/jakubbonczyk/todoapp/model/Task.java
+++ src/main/java/github/com/jakubbonczyk/todoapp/model/Task.java
@@ -1,9 +1,38 @@
 package github.com.jakubbonczyk.todoapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+    @Id
+    private int id;
+    private int description;
+    private boolean isDone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDescription() {
+        return description;
+    }
+
+    public void setDescription(int description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
 }
